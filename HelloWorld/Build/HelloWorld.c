a3840437071b3def58934d44fc736e7111bdbb13
Lars Voegtle <voegtlel@emea.roche.com>
2020-05-05 10:07:53 +0200

    Hello World files

---
+++ HelloWorld/Build/HelloWorld.c
@@ -0,0 +1,20 @@
+#include <avr/io.h>
+#include <Atmega328P.h>
+#include <AvrLib.h>
+#include <RegisterAccess.h>
+#define F_CPU 16000000
+#include <util/delay.h>
+#include <inttypes.h>
+#include <avr/interrupt.h>
+
+int main(void)
+{
+	Usart_Init(250000);
+
+	while (True)
+	{
+		Usart_Trace0(1);
+			_delay_ms(1000);
+	}
+	return 0;
+}
